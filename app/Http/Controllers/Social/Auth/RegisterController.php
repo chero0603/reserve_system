73536045b27622e9733967e67f61cf7ec247fd10
chero0603 <chero@grooow.com>
2020-03-25 14:22:37 +0900

    Auth japanese localized fixed

--- app/Http/Controllers/Social/Auth/RegisterController.php
+++ app/Http/Controllers/Social/Auth/RegisterController.php
@@ -124,7 +124,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make(Str::random(30)),
         ]);
     }
 }
